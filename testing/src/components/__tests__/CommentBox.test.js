3de0af7794cba9ecb94585701ed8c73b3a72447a
JarekSopinski <jarek.sopinski@gmail.com>
2023-02-10 09:40:35 +0100

    Simulating Textarea Event

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -17,3 +17,22 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+/**
+ * Simulating event:
+ * 1. Find the textarea element
+ * 2. Simulate a 'change' event
+ * 3. Provide a fake event object
+ * 4. Force the component to update
+ * 5. Assert that the textarea's value has changed
+ */
+
+it('has a text area that users can type in', () => {
+    const simulatedValue = 'new comment';
+    wrapped.find('textarea').simulate('change', {
+        target: { value: simulatedValue }
+    });
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual(simulatedValue);
+});
