0c10875b2da8572f38f0feb62ab6c383df550a19
JarekSopinski <jarek.sopinski@gmail.com>
2023-02-13 15:03:00 +0100

    Creating the HOC

---
+++ testing/src/components/requireAuth.js
@@ -0,0 +1,12 @@
+// This is a HOC!
+import React, { Component } from 'react';
+
+export default (ChildComponent) => {
+    class ComposedComponent extends Component {
+        render() {
+            return <ChildComponent />
+        }
+    }
+
+    return ComposedComponent;
+};
