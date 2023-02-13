4a4139c1ded81abf6715393395c5d02500d417c1
JarekSopinski <jarek.sopinski@gmail.com>
2023-02-13 15:19:21 +0100

    Passing Through Props

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import requireAuth from './requireAuth.js';
 
 class CommentBox extends Component {
 
@@ -34,4 +35,4 @@ class CommentBox extends Component {
 
 
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
